96e0bfdca470f21498891132af3fadc3acc4b327
AEP <91467886+TukangM@users.noreply.github.com>
2024-01-26 21:30:52 +0700

    Update customize.sh
    fix customize.sh

--- customize.sh
+++ customize.sh
@@ -25,7 +25,7 @@ mkdir -p "/data/local/pmmp"
 
 # Menyalin seluruh folder pmmp
 ui_print "Copy all files to pmmp"
-cp -r "$MODDIR/pmmp" "/data/local/pmmp"
+# cp -r "$MODDIR/pmmp" "/data/local/pmmp" get cancelled
 
 # Berikan izin eksekusi secara rekursif pada seluruh folder pmmp
 ui_print "Giving chmod permission"
@@ -41,7 +41,7 @@ cp "$MODDIR/start-pmmp" "/system/bin"
 
 # (Opsional) Berikan izin eksekusi pada start-pmmp
 ui_print "Giving chmod permission for start-pmmp"
-chmod +x "/system/bin/start-pmmp"
+chmod +x "$MODDIR/system/start-pmmp"
 
 # Teks tambahan
 ui_print "Done - Reboot to take effect"
