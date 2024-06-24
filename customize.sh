50c4f6be625da5db7c22135422ae2c6470dfd995
AEP <91467886+TukangM@users.noreply.github.com>
2024-06-24 23:38:21 +0700

    modify customize.sh
    Trying fixing Error:
    
    customize.sh:
    
    line 7:
    
    syntax
    
    error:
    
    unexpected

--- customize.sh
+++ customize.sh
@@ -3,13 +3,6 @@
 MODDIR=${0%/*}
 
 # Teks ASCII
-ui_print "  ____            _        _   __  __ _                  __  __ ____  _  _     _              _           _     _ "
-ui_print " |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \| || |   / \   _ __   __| |_ __ ___ (_) __| |"
-ui_print " | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) | || |_ / _ \ | '_ \ / _` | '__/ _ \| |/ _` |"
-ui_print " |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/|__   _/ ___ \| | | | (_| | | | (_) | | (_| |"
-ui_print " |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|      |_|/_/   \_\_| |_|\__,_|_|  \___/|_|\__,_|"
-ui_print "                                                                                                                  "
-ui_print "                                                                                                                  "
 ui_print "                              https://github.com/TukangM/PocketMine-MP4Android                                    "
 ui_print "                                   Made with AI generated chat.openai.com                                         "
 ui_print "                      https://chat.openai.com/share/8f541b0c-947d-438a-a7c1-18ac00054a93                          "
